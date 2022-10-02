8a5a1a92329a2c4837eed658ef156ea9a38f4989
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2022-10-02 13:16:15 +0300

    Initial Commit

---
+++ ContactListSwiftUI/ContactListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSwiftUIApp.swift
+//  ContactListSwiftUI
+//
+//  Created by Lera Savchenko on 2.10.22.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
